c4b1211f4e16ee8aea856143c0cb328db2e61dcf
alex <eafo72@gmail.com>
2024-04-11 17:45:38 -0600

    11042024

--- src/pages/Registry.js
+++ src/pages/Registry.js
@@ -1,18 +1,23 @@
 import React from "react";
 
+import  InstagramLogin from "@amraneze/react-instagram-login"
+
 function Registry() {
-  
-  
- 
-
-  
-
- 
+  const responseInstagram = (response) => {
+    console.log(response);
+  };
 
   return (
     <main>
       <div align="center">
         <h3>Registry</h3>
+
+        <InstagramLogin
+          clientId="451501287304003"
+          buttonText="Login"
+          onSuccess={responseInstagram}
+          onFailure={responseInstagram}
+        />
       </div>
     </main>
   );
