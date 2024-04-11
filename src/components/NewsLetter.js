7d384df7beb2e9849855b3d563db6fc8ce716cb6
alex <eafo72@gmail.com>
2024-04-11 17:02:42 -0600

    11042024

--- src/components/NewsLetter.js
+++ src/components/NewsLetter.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { React, useEffect } from "react";
+
+import axios from "axios";
+
+import clienteAxios from "../config/axios";
+
+import { toast } from "sonner";
 
 function Newsletter() {
   const client_id = "451501287304003";
+  const client_secret = "7253909d53e1ec5617c5e30de36cf4ce";
   const redirect_uri = "https://landing.flagasamascotas.com";
   const scope = "user_profile"; // Requested scope
 
@@ -11,6 +18,79 @@ function Newsletter() {
 
     // Redirect the user to the authorization URL
     window.location.href = authUrl;
+  };
+
+  const fetchUserProfile = async (code) => {
+    const config = {
+      url: "https://api.instagram.com/oauth/access_token",
+      method: "POST",
+      data: JSON.stringify({
+        client_id,
+        client_secret,
+        grant_type: "authorization_code",
+        redirect_uri,
+        code: code,
+      }),
+    };
+
+    try {
+      const response = await axios(config);
+
+      console.log(response);
+
+      const { access_token } = response.data;
+      console.log("AC" + access_token);
+
+      /*
+        // Fetch user profile using the access token
+        const profileResponse = await axios.get(
+          `https://graph.instagram.com/me?fields=id,username&access_token=${access_token}`
+        );
+  
+        //info del usuario
+        console.log(profileResponse.data);
+      */
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+    }
+  };
+
+  const handleAuthCallback = () => {
+    const code = new URLSearchParams(window.location.search).get("code");
+    if (code) {
+      fetchUserProfile(code);
+    }
+  };
+
+  // Check if the URL contains an authorization code on component mount
+  useEffect(() => {
+    handleAuthCallback();
+  }, []);
+
+  const mostrarMensaje = (mensaje) => {
+    toast.error(mensaje);
+  };
+
+  const mostrarAviso = (mensaje) => {
+    toast.success(mensaje);
+  };
+
+  const sendData = async () => {
+    try {
+      const res = await clienteAxios.get(`/registrar.php`);
+      console.log(res);
+
+      /*
+			setTitle(res.data.single.title);
+			setWords(res.data.single.words);
+			setDescription(res.data.single.description);
+			*/
+
+      mostrarAviso("¡FELICIDADES! has quedado registrado.");
+    } catch (error) {
+      mostrarMensaje(error);
+      console.log(error);
+    }
   };
 
   return (
